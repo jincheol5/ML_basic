ad8cc4c0953377ee686e265c20f6c15445275c18
jincheol <oscar4293@gmail.com>
2024-08-01 23:13:35 +0900

    update

---
+++ DL/RNN/app_lstm.py
@@ -0,0 +1,52 @@
+from lstm import CustomLSTM
+from custom_dataset import StockDataProcess
+from model_optimization import Optimization
+
+import os
+import numpy as np
+import random
+import torch
+import torch.nn as nn
+
+
+
+# random seed
+random_seed= 42
+random.seed(random_seed)
+np.random.seed(random_seed)
+os.environ["PYTHONHASHSEED"] = str(random_seed)
+torch.manual_seed(random_seed)
+torch.cuda.manual_seed(random_seed)
+torch.cuda.manual_seed_all(random_seed)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+torch.use_deterministic_algorithms(False)
+
+# GPU 사용 가능한지 확인
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    print("GPU is available\n")
+else:
+    device = torch.device("cpu")
+    print("GPU is not available, using CPU instead\n")
+
+# hyperparameter
+lr=0.01
+batch_size=100
+epochs=100
+
+### custom RNN
+# model
+custom_model=CustomLSTM(input_dim=5,hidden_dim=10,output_dim=1).to(device)
+
+# get DataLoader
+SDP=StockDataProcess(seq_length=7)
+train_DataLoader,test_DataLoader=SDP.get_DataLoader(batch_size=batch_size) 
+
+# train model
+optimization=Optimization(model=custom_model,epochs=epochs,lr=lr)
+optimization.train_model(dataloader=train_DataLoader,loss_fn=nn.MSELoss())
+
+# test model
+optimization.test_model(dataloader=test_DataLoader,loss_fn=nn.MSELoss())
+
---
+++ DL/RNN/lstm.py
@@ -0,0 +1,93 @@
+import torch
+import torch.nn as nn
+
+
+class CustomLSTMCell(nn.Module):
+    def __init__(self,input_dim,hidden_dim):
+        super().__init__()
+        self.input_dim=input_dim
+        self.hidden_dim=hidden_dim
+
+        # forget gate
+        self.W_hf=nn.Linear(self.hidden_dim,self.hidden_dim)
+        self.W_xf=nn.Linear(self.input_dim,self.hidden_dim)
+
+        # input gate
+        self.W_hi=nn.Linear(self.hidden_dim,self.hidden_dim)
+        self.W_xi=nn.Linear(self.input_dim,self.hidden_dim)
+
+        # candidate cell state gate 
+        self.W_hg=nn.Linear(self.hidden_dim,self.hidden_dim)
+        self.W_xg=nn.Linear(self.input_dim,self.hidden_dim)
+
+        # output gate
+        self.W_ho=nn.Linear(self.hidden_dim,self.hidden_dim)
+        self.W_xo=nn.Linear(self.input_dim,self.hidden_dim)
+
+        self.act_tanh=nn.Tanh()
+        self.act_sigmoid=nn.Sigmoid()
+
+    def forward(self,x_seq):
+        # input x_seq = [batch_size,seq_len,input_dim]
+
+        batch_size,seq_len,input_dim=x_seq.size()
+
+        device=x_seq.device # Ensure all tensors are on the correct device
+
+        h_0=torch.zeros(self.hidden_dim,device=device)
+
+        c_0=torch.zeros(self.hidden_dim,device=device)
+
+        output_h_t=torch.zeros(batch_size,self.hidden_dim,device=device)
+
+        for batch in range(batch_size):
+
+            prev_h_t=h_0
+            prev_c_t=c_0
+
+            h_t=h_0
+            c_t=c_0
+
+            for t in range(seq_len):
+                
+                # forget gate
+                f_t=self.act_sigmoid(self.W_hf(prev_h_t)+self.W_xf(x_seq[batch][t]))
+                
+                # input gate
+                i_t=self.act_sigmoid(self.W_hi(prev_h_t)+self.W_xi(x_seq[batch][t]))
+                
+                # candidate cell state
+                g_t=self.act_tanh(self.W_hg(prev_h_t)+self.W_xg(x_seq[batch][t]))
+
+                # output gate
+                o_t=self.act_sigmoid(self.W_ho(prev_h_t)+self.W_xo(x_seq[batch][t]))
+
+                # cell state
+                c_t=f_t*prev_c_t+i_t*g_t # 요소별 합과 요소별 곱 
+                
+                # output
+                h_t=o_t*self.act_tanh(c_t)
+
+                prev_h_t=h_t
+                prev_c_t=c_t
+
+            output_h_t[batch]=h_t
+
+        return output_h_t
+
+
+# many-to-one rnn model
+class CustomLSTM(nn.Module):
+    def __init__(self,input_dim,hidden_dim,output_dim):
+        super().__init__()
+        self.input_dim=input_dim
+        self.hidden_dim=hidden_dim
+        self.output_dim=output_dim
+
+        self.lstm_cell=CustomLSTMCell(self.input_dim,self.hidden_dim)
+        self.fc=nn.Linear(self.hidden_dim,self.output_dim)
+
+    def forward(self,x_seq):
+        # input x_seq = [batch_size,seq_len,input_dim] 형태이다. 
+        hidden_state=self.lstm_cell(x_seq)
+        return self.fc(hidden_state)

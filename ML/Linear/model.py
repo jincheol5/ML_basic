058ca6252666aedd797e137feb02049b4d98fa86
jincheol5 <oscar4293@gmail.com>
2025-02-09 00:59:26 +0900

    update

--- ML/Linear/model.py
+++ ML/Linear/model.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 
 class CustomLinear(nn.Module):
     def __init__(self,input_dim,hidden_dim,output_dim):
+        super.__init__()
         self.layer=nn.Sequential(
             nn.Linear(in_features=input_dim,out_features=hidden_dim),
             nn.Linear(in_features=hidden_dim,out_features=output_dim),

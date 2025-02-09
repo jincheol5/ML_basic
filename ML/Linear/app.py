a7ae7fa1da2848012ced3468a72db92046853137
jincheol5 <oscar4293@gmail.com>
2025-02-09 23:05:33 +0900

    update

--- ML/Linear/app.py
+++ ML/Linear/app.py
@@ -1,22 +1,32 @@
 import os
 import random
 import numpy as np
+import wandb
 from model import CustomLinear
 from dataset import CustomDataset
 from model_train import Model_Trainer
 import torch
 from torch.utils.data import random_split,DataLoader
+"""
+wandb
+"""
+wandb.init(project='ML_basic',config={
+    'seed':42,
+    'lr':0.0005,
+    'epochs':100,
+    'batch_size':32
+})
+config=wandb.config
 
 """
 seed setting
 """
-seed=42
-random.seed(seed)
-np.random.seed(seed)
-torch.manual_seed(seed) 
-os.environ["PYTHONHASHSEED"]=str(seed)
-torch.cuda.manual_seed(seed)
-torch.cuda.manual_seed_all(seed)
+random.seed(config['seed'])
+np.random.seed(config['seed'])
+torch.manual_seed(config['seed']) 
+os.environ["PYTHONHASHSEED"]=str(config['seed'])
+torch.cuda.manual_seed(config['seed'])
+torch.cuda.manual_seed_all(config['seed'])
 torch.backends.cudnn.deterministic=True 
 torch.backends.cudnn.benchmark=False
 
@@ -29,12 +39,12 @@ dataset=CustomDataset(x=x,y=y)
 train_size=int(0.9*len(dataset))
 test_size=len(dataset)-train_size
 train_dataset,test_dataset=random_split(dataset=dataset,lengths=[train_size,test_size])
-train_data_loader=DataLoader(dataset=train_dataset,batch_size=32,shuffle=True)
-test_data_loader=DataLoader(dataset=test_dataset,batch_size=32,shuffle=True)
+train_data_loader=DataLoader(dataset=train_dataset,batch_size=config['batch_size'],shuffle=True)
+test_data_loader=DataLoader(dataset=test_dataset,batch_size=config['batch_size'],shuffle=True)
 
 """
 model set and train
 """
 model=CustomLinear(input_dim=1,output_dim=1)
-Model_Trainer.train(model=model,data_loader=train_data_loader,lr=0.01,epochs=100)
+Model_Trainer.train(model=model,data_loader=train_data_loader,config=config)
 Model_Trainer.test(model=model,data_loader=test_data_loader)
--- ML/Linear/model_train.py
+++ ML/Linear/model_train.py
@@ -1,26 +1,31 @@
 from tqdm import tqdm
 import numpy as np
+import wandb
 import torch
 from torch.nn import MSELoss
 
 class Model_Trainer:
     @staticmethod
-    def train(model,data_loader,lr=0.0005,epochs=10):
+    def train(model,data_loader,config):
         device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        optimizer=torch.optim.Adam(model.parameters(),lr=lr)
+        optimizer=torch.optim.Adam(model.parameters(),lr=config['lr'])
         model.to(device)
         model.train()
         criterion=MSELoss()
-        for epoch in tqdm(range(epochs),desc=f"model training..."):
+        for epoch in tqdm(range(config['epochs']),desc=f"model training..."):
+            sum_batch_loss=0.0
             for batch in data_loader:
                 inputs,labels=batch
                 inputs=inputs.to(device)
                 labels=labels.to(device)
                 outputs=model(x=inputs)
                 loss=criterion(outputs,labels)
+                sum_batch_loss+=loss.item()
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
+            epoch_loss=sum_batch_loss/len(data_loader)
+            wandb.log({"Training MSE Loss": epoch_loss},step=epoch)
 
     @staticmethod
     def test(model,data_loader):
@@ -30,7 +35,6 @@ class Model_Trainer:
         criterion=MSELoss()
         total_loss=[]
         with torch.no_grad():
-            mse_loss_list=[]
             for batch in tqdm(data_loader,desc=f"model evaluating..."):
                 inputs,labels=batch
                 inputs=inputs.to(device)
